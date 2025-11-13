43623e68c86fb5bc654f4acb7cf16ddaf04f18a4
nyp3r <bjarke.julius@gmail.com>
2025-11-13 07:41:54 +0100

    house items added

--- Scripts/escape.gd
+++ Scripts/escape.gd
@@ -1,7 +1,6 @@
 extends RayCast2D
 
-func _physics_process(delta: float) -> void:
-	var collider = get_collider()
+func _physics_process(_delta: float) -> void:
 	if is_colliding():
 		var player = get_tree().get_first_node_in_group("Player") as Player
 		if get_collider().name == player.name and player.has_teddy_bear:
