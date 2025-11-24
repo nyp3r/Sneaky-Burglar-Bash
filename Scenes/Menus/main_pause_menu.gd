941fc0dde28f71241374836fb4c101eda7698a4c
Nyp3r <you@example.com>
2025-11-24 21:34:28 +0100

    ingame settings menu

--- Scenes/Menus/main_pause_menu.gd
+++ Scenes/Menus/main_pause_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var canvas_layer: CanvasLayer = $CanvasLayer
+@onready var canvas_layer: CanvasLayer = $".."
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
